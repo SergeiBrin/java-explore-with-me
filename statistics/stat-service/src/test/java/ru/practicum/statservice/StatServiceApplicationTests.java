b8f6c182b1125d919949a4be6db03bde3c59f8b8
Sergei <starodumovtmt@yandex.ru>
2023-07-22 12:04:47 +0500

    style: make code cleaner

--- statistics/stat-service/src/test/java/ru/practicum/statservice/StatServiceApplicationTests.java
+++ statistics/stat-service/src/test/java/ru/practicum/statservice/StatServiceApplicationTests.java
@@ -2,8 +2,6 @@ package ru.practicum.statservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 class StatServiceApplicationTests {
